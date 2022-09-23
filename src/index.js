659d938e09c33fc0bb1398c529d2df3912c52abf
Hilário Grossi de Oliveira <hilariogrossi@yahoo.com.br>
2022-09-23 19:10:02 -0300

    Atualização 23/09/2022 19:09

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Pascal Case: a segunda palavra do componente é em caixa alta.
+function App() {
+    return <SubTitle />
+}; // Todos os componentes do React devem ser declarados com sua
+// primeira letra do nome sendo maiúscula.
+// Convenções não são regras, são apenas padrões que a comunidade adota
+// e decide seguir.
+// Nesse caso não se chama a função App(). Usa-se dentro do render <App />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function SubTitle() {
+    return <h2>Componente Subtítulo!</h2>
+}; // Componentes são funções JavaScript que precisam começar com a primeira letra maiúscula e retornar o código JSX que são os elementos que serão exibidos na tela quando o componente for usado.
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root'),
+);                  // Pegar o código aqui e fazer aparecer na tela
+                    // do usuário.
+                    // Render é o processo de converter o código em algo visual, na interface que o usuário verá.
