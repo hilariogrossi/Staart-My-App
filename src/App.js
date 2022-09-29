0a2d5d6f590d41070c41ab49903cab2ed8b85d16
Hilário Grossi de Oliveira <hilariogrossi@yahoo.com.br>
2022-09-29 13:52:36 -0300

    Atualização 29/09/2022 13:52

--- src/App.js
+++ src/App.js
@@ -8,9 +8,8 @@ import './styles/Feed.css';
 
 import userIcon from './images/user.svg';
 import paperPlaneIcon from './images/paper-plane.svg';
-import clockIcon from './images/clock.svg';
-import emptyFolderIcon from './images/empty-folder.svg';
 
+import Feed from './components/feed.js'
 
 // Pascal Case: a segunda palavra do componente é em caixa alta.
 export default function App() {
@@ -69,54 +68,7 @@ export default function App() {
             </form>
 
             <main>
-                {posts.length === 0 && (
-                    <div className='feed-status'>
-                        <img src={emptyFolderIcon} alt='Empty Folder' />
-                        <h1>Não encontramos nada.</h1>
-                        <h2>
-                            Parece que você e seus amigos não postaram nada. Comece a escrever uma nova história!
-                        </h2>
-
-                    </div>
-                )}
-
-
-                {/* {posts.length > 0 ? <header>
-                    <h1>Seu Feed</h1>
-                    <h2>Acompanhe o que seus amigos estão pensando em tempo real.</h2>
-                </header> : null*/}
-
-                {posts.length > 0 && (
-                    <>
-                        <header>
-                            <h1>Seu Feed</h1>
-                            <h2>Acompanhe o que seus amigos estão pensando em tempo real.</h2>
-                        </header>
-
-                        <section className='feed'>
-                            {posts.map((post) => (
-                                <article key={post.id}>
-                                    <p>
-                                        {post.content}
-                                    </p>
-
-                                    <footer>
-                                        <div className='user-details'>
-                                            <img src={userIcon} alt="User" />
-                                            <strong> {post.userName} </strong>
-                                        </div>
-
-                                        <div className='time'>
-                                            <img src={clockIcon} alt="Clock" />
-                                            <span>Publicado em {post.publishedAt.toLocaleDateString('pt-br')} </span>
-                                        </div>
-                                    </footer>
-                                </article>
-                            ))}
-
-                        </section>
-                    </>
-                )}
+                <Feed posts={posts} />
 
             </main>
         </div>); /* Dentro da div para fazer css =  style={{
---
+++ src/components/feed.js
@@ -0,0 +1,57 @@
+import clockIcon from '../images/clock.svg';
+import emptyFolderIcon from '../images/empty-folder.svg';
+import userIcon from '../images/user.svg';
+
+export default function Feed(props) {
+    if (props.posts.lenght === 0) {
+        return (
+            <div className='feed-status'>
+                <img src={emptyFolderIcon} alt='Empty Folder' />
+                <h1>Não encontramos nada.</h1>
+                <h2>
+                    Parece que você e seus amigos não postaram nada. Comece a escrever uma nova história!
+                </h2>
+
+            </div>
+
+            /* {posts.length > 0 ? <header>
+    <h1>Seu Feed</h1>
+    <h2>Acompanhe o que seus amigos estão pensando em tempo real.</h2>
+    </header> : null*/
+
+        );
+
+    } else {
+        return (
+            <>
+                <header>
+                    <h1>Seu Feed</h1>
+                    <h2>Acompanhe o que seus amigos estão pensando em tempo real.</h2>
+                </header>
+
+                <section className='feed'>
+                    {props.posts.map((post) => (
+                        <article key={post.id}>
+                            <p>
+                                {post.content}
+                            </p>
+
+                            <footer>
+                                <div className='user-details'>
+                                    <img src={userIcon} alt="User" />
+                                    <strong> {post.userName} </strong>
+                                </div>
+
+                                <div className='time'>
+                                    <img src={clockIcon} alt="Clock" />
+                                    <span>Publicado em {post.publishedAt.toLocaleDateString('pt-br')} </span>
+                                </div>
+                            </footer>
+                        </article>
+                    ))}
+
+                </section>
+            </>
+        );
+    };
+};
